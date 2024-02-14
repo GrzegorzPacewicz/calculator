a04b8ead4f223f83bb0d2633f49b9d8cbcb72acd
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-14 20:41:20 +0100

    Display and Keypad added to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import { Display } from "./components/Display/Display";
+import { Keypad } from "./components/Keypad/Keypad";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className={styles.calculatorView}>
         <div className={styles.viewport}>
           <Display />
+          <Keypad />
         </div>
       </div>
     </>
