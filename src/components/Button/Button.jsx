6c088f880cd49a6df41984d394756a54b13ac4f7
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-16 20:10:27 +0100

    Buttons set

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Button.module.css";
-
-export function Button({ value, handleButtonClick, label }) {
-  return (
-    <button name={value} className={styles.button} onClick={handleButtonClick}>
-      {label}
-    </button>
-  );
-}
