4ce6ee577cf4be121601774f1c34407d4d7df019
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-16 20:10:37 +0100

    Basic Keypad set

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,14 +1,30 @@
+import { FunctionButton, OpButton, SpecialButton } from "../Button/Buttons";
 import styles from "./Keypad.module.css";
 
-export function Keypad() {
+export function Keypad(props) {
+  const { actionToPerform, allClear } = props;
+  const handleClickButton = (value, keyType) => {
+    actionToPerform(value, keyType);
+  };
+
+  const functionKeys = [
+    { label: allClear ? "AC" : "C", value: allClear ? "AC" : "C" },
+    { label: "±", value: "+/-" },
+    { label: "%", value: "%" },
+  ];
+
   return (
     <div className={styles.container}>
-      <button className={styles.fxKey}>1</button>
-      <button className={styles.numericKey}>1</button>
-
-      <div className={styles.operators}>
-        <button className={styles.opKey}>1</button>
-      </div>
+      <div className={styles.functionButtons}></div>
+      {functionKeys.map((functionKey) => (
+        <FunctionButton
+          key={functionKey.label}
+          label={functionKey.label}
+          value={functionKey.value}
+          onClick={handleClickButton}
+        />
+      ))}
+      <div className={styles.operators}></div>
     </div>
   );
 }
