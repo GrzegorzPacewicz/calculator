20aa3c045987490f70764a0ba571db27fe74a7ef
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-16 07:22:23 +0100

    Buttons hard coded

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,14 +1,14 @@
-import { Button } from "../Button/Button";
 import styles from "./Keypad.module.css";
 
 export function Keypad() {
   return (
     <div className={styles.container}>
-      <div className={styles.keys}>
-        <Button label={2} />
-      </div>
+      <button className={styles.fxKey}>1</button>
+      <button className={styles.numericKey}>1</button>
 
-      <div className={styles.operators}>Operators</div>
+      <div className={styles.operators}>
+        <button className={styles.opKey}>1</button>
+      </div>
     </div>
   );
 }
