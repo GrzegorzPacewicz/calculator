30d605806feeb3e2c0ba63d2fbccc908a2a40250
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-18 16:22:10 +0100

    Placeholder removed. Usestates added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import { Display } from "./components/Display/Display";
 import { Keypad } from "./components/Keypad/Keypad";
 
 function App() {
+  const [accValue, setAccValue] = useState(null);
+  const [screenValue, setScreenValue] = useState("0");
+  const [currentOperator, setCurrentOperator] = useState(null);
+  const [expectsOperand, setExpectsOperand] = useState(false);
+
   const handleActionToPerform = () => {};
   const isScreenClear = () => {};
 
@@ -10,7 +16,7 @@ function App() {
     <>
       <div className={styles.calculatorView}>
         <div className={styles.viewport}>
-          <Display />
+          <Display value={screenValue} />
           <Keypad
             actionToPerform={handleActionToPerform}
             allClear={isScreenClear}
--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -3,13 +3,7 @@ import styles from "./Display.module.css";
 export function Display({ value }) {
   return (
     <div className={styles.display}>
-      <input
-        className={styles.input}
-        type="text"
-        tabIndex={-1}
-        value={value}
-        placeholder="0"
-      />
+      <input className={styles.input} type="text" tabIndex={-1} value={value} />
     </div>
   );
 }
