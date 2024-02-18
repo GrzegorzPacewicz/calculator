ec06b7f542e36feb4c61450de10096943221539c
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-18 17:19:00 +0100

    Display input is readonly

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -3,7 +3,13 @@ import styles from "./Display.module.css";
 export function Display({ value }) {
   return (
     <div className={styles.display}>
-      <input className={styles.input} type="text" tabIndex={-1} value={value} />
+      <input
+        className={styles.input}
+        type="text"
+        tabIndex={-1}
+        value={value}
+        readOnly
+      />
     </div>
   );
 }
